bb0787d89848046a5bbb9e3722562c850c784d78
Filipp Kramer <krame490@gmail.com>
2020-01-04 00:37:34 -0600

    add fs logging

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -1,6 +1,25 @@
 use clap::{App, Arg};
+use slog::{Drain, *};
+use slog_async;
+use slog_term;
+
+use std::fs::OpenOptions;
 
 fn main() {
+    let log_path = "target/devel-unified.log";
+    let file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(log_path)
+        .unwrap();
+
+    let decorator = slog_term::PlainDecorator::new(file);
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    let _log = slog::Logger::root(drain, o!());
+
     let matches = App::new("Kvs Server")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
