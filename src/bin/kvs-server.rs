65fa12b9e14d8dded88332deeee5a69c85b3c19a
Filipp Kramer <krame490@gmail.com>
2020-01-04 14:57:36 -0600

    add error and info version, addr, eng logs

--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -18,8 +18,11 @@ fn main() {
     let drain = slog_term::FullFormat::new(decorator).build().fuse();
     let drain = slog_async::Async::new(drain).build().fuse();
 
-    let _log = slog::Logger::root(drain, o!());
+    let _log = slog::Logger::root(drain, o!("key1" => "value1", "key2" => "value2"));
 
+    info!(_log, "Server version number: {}", env!("CARGO_PKG_VERSION"));
+    error!(_log, "{}", env!("CARGO_PKG_VERSION"));
+    eprintln!("error log");
     let matches = App::new("Kvs Server")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
@@ -40,10 +43,18 @@ fn main() {
         .get_matches();
 
     if let Some(addr) = matches.value_of("address") {
-        println!("received an address: {}", addr)
+        info!(_log, "Kvs-Server socket address is: {}", addr);
+        error!(_log, "Kvs-Server socket address is: {}", addr);
+    } else {
+        info!(_log, "no process provided. Defaulting to 4000");
+        error!(_log, "running on port 4000");
     }
 
     if let Some(engine) = matches.value_of("engine") {
+        info!(_log, "server running with: {} engine", engine);
         println!("received an engine: {}", engine)
+    } else {
+        info!(_log, "server running with: kvs engine");
+        error!(_log, "server running with: kvs engine");
     }
 }
