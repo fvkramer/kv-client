60fc758c0300954afbdbee34c882c7033e095e36
Filipp Kramer <krame490@gmail.com>
2020-01-06 20:22:28 -0800

    add terminal clear and prompt

--- src/bin/kvs-client.rs
+++ src/bin/kvs-client.rs
@@ -1,10 +1,14 @@
 use std::io::prelude::*;
-use std::io::Result;
+use std::io::{self, Result};
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpStream};
+use std::process::Command;
 use std::time::Duration;
 
 fn main() -> Result<()> {
     let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 4000));
+    Command::new("clear").spawn()?.wait();
+
+    run_command_prompt();
     if let Ok(mut stream) = TcpStream::connect(&addr) {
         println!("local addr: {}", stream.local_addr()?);
         println!("remote addr: {}", stream.peer_addr()?);
@@ -13,6 +17,19 @@ fn main() -> Result<()> {
         stream.write(&msg[..]);
     } else {
         println!("Couldn't connect to server...");
+    }
+
+    Ok(())
+}
+
+fn run_command_prompt() -> Result<()> {
+    let mut input = String::new();
+
+    loop {
+        print!("> ");
+        io::stdout().flush();
+
+        let term_input = io::stdin().read_line(&mut input);
     }
 
     Ok(())
